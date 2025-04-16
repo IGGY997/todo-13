c6aa6f6ce034c8f763eedae0365fdf73d879f921
IGGY997 <ignatius.kamau@student.moringaschool.com>
2025-04-16 10:52:23 +0300

    Fix import paths for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'src/App.css'
-import App from 'src/App.jsx'
+import './App.css'
+import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
