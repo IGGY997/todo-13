a9952b1f2f8e400ae65aaf52a914aef5c94c078a
IGGY997 <ignatius.kamau@student.moringaschool.com>
2025-04-16 10:41:11 +0300

    Fix Vite config for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
+import 'src/assets/App.css'
+import App from 'src/assets/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
