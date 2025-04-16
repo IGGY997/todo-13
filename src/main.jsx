c1bf98725dc2b09c0b1fee43cafb67575fd32d0e
IGGY997 <ignatius.kamau@student.moringaschool.com>
2025-04-16 10:48:58 +0300

    Fix import path for App.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'src/assets/App.css'
-import App from 'src/assets/App.jsx'
+import 'src/App.css'
+import App from 'src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
